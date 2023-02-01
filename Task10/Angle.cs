0efaf780121ff7b914391c182d94650edf9c1b76
Алёна Алёна <alenchaeto1@outlook.com>
2023-02-01 09:40:19 +0300

    Добавьте файлы проекта.

---
+++ Task10/Angle.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Angle
+    {
+        private int gradus;
+        private int minute;
+        private int second;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = -value;
+                }
+                if (value + gradus > 360)
+                {
+                    value += gradus;
+                    gradus = value % 360;
+                }
+                else
+                {
+                    gradus += value;
+                }
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = -value;
+                }
+                if (value + minute > 60)
+                {
+                    value += minute;
+                    gradus += value / 60;
+                    minute = value % 60;
+                }
+                else
+                {
+                    minute += value;
+                }
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = -value;
+                }
+                if (value > 60)
+                {
+                    minute += value / 60;
+                    second += value % 60;
+                }
+                else
+                {
+                    second += value;
+                }
+            }
+        }
+
+        public Angle(int gradus, int minute, int second)
+        {
+            this.Second = second;
+            this.Minute = minute;
+            this.Gradus = gradus;
+        }
+
+        public double ToRadians()
+        {
+            return (gradus + (minute + second / 60) / 60) * Math.PI / 180;
+        }
+    }
+
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительные значения. Отрицательные значения будут менять знак на +");
+            Console.WriteLine("Градусов:");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Минут:");
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Секунд:");
+            int second_ = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus_, minute_, second_);
+
+            Console.WriteLine("{0} градусов, {1} минут, {2} секунд", angle.Gradus, angle.Minute, angle.Second);
+            
+            double radian = angle.ToRadians();
+            Console.WriteLine(radian);
+            Console.ReadKey();
+
+        }
+       
+    }
+}
